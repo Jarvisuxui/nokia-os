10f4503e9285d0a674702e9c3798e4fc7166050e
Jarvisuxui <joseacostabrizuela@gmail.com>
2026-09-17 18:32:56 +0200

    Bump Nokia OS cache to v0.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nokia-os-v04';
+const CACHE='nokia-os-v05';
 const STATIC=['./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   self.skipWaiting();
